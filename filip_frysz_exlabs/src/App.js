ec8ccc11d300c3a5c471d0623e3873c1450a112d
Filip Frysz <ffrysz@gmail.com>
2021-12-20 00:00:36 +0100

    Add first component - Header

--- filip_frysz_exlabs/src/App.js
+++ filip_frysz_exlabs/src/App.js
@@ -1,47 +1,50 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import { useQuery } from 'react-query';
 
-export default function App() {
+import Header from './components/Header/Header';
 
-  const url = 'https://api.spacex.land/graphql/';
-  const SPACEX_QUERY = `{
-      launchesPast(limit: 10) {
-        mission_name
-        launch_date_local
-        launch_site {
-          site_name_long
-        }
-        links {
-          article_link
-          video_link
-        }
-        rocket {
-          rocket_name
-          first_stage {
-            cores {
-              flight
-              core {
-                reuse_count
-                status
-              }
-            }
-          }
-          second_stage {
-            payloads {
-              payload_type
-              payload_mass_kg
-              payload_mass_lbs
+const url = 'https://api.spacex.land/graphql/';
+const SPACEX_QUERY = `{
+    launchesPast(limit: 10) {
+      mission_name
+      launch_date_local
+      launch_site {
+        site_name_long
+      }
+      links {
+        article_link
+        video_link
+      }
+      rocket {
+        rocket_name
+        first_stage {
+          cores {
+            flight
+            core {
+              reuse_count
+              status
             }
           }
         }
-        ships {
-          name
-          home_port
-          image
+        second_stage {
+          payloads {
+            payload_type
+            payload_mass_kg
+            payload_mass_lbs
+          }
         }
       }
-    }`;
+      ships {
+        name
+        home_port
+        image
+      }
+    }
+  }`;
+
+
+export default function App() {
 
   const { data, isLoading, error } = useQuery("launches", () => {
     return fetch(url, {
@@ -56,14 +59,17 @@ export default function App() {
           return response.json();
         }
       })
-      .then((data) => console.log(data));
+      .then((data) => data.data);
   });
 
+  if (isLoading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>;
 
   return (
     <div className="App" >
-    </div>
+      <Header />
+      <span>{data.launchesPast[0].mission_name}</span>
+    </div >
   );
 }
 
-// export default App;
