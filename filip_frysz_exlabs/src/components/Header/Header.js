c790e7e0e25f4f74a3e4aa1b582b6519fb02509e
Filip Frysz <ffrysz@gmail.com>
2021-12-20 20:18:01 +0100

    Add useEffect hook to Header component

--- filip_frysz_exlabs/src/components/Header/Header.js
+++ filip_frysz_exlabs/src/components/Header/Header.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Header.css';
 import Logo from './Group.svg';
 
 const Header = (props) => {
 
-  const { prev, next } = props;
+  const { prev, next, current } = props;
+
+
+  useEffect(() => {
+    const buttonNext = document.querySelector('#buttonNext');
+    const buttonPrev = document.querySelector('#buttonPrev');
+
+    if (current > 0) {
+      buttonPrev.classList.add('button-available');
+    } else if (current === 0) {
+      buttonPrev.classList.remove('button-available');
+    }
+    if (current < 4) {
+      buttonNext.classList.add('button-available');
+    } else if (current === 4) {
+      buttonNext.classList.remove('button-available');
+    }
+  });
 
   return (
     <div className='header'>
-      <button className='button-change-launch' onClick={prev}>
+      <button className='button-change-launch' onClick={prev} id='buttonPrev'>
         <i className="fas fa-chevron-right previous"></i>
       </button>
       <img src={Logo} alt='Logo' />
-      <button className='button-change-launch' onClick={next}>
+      <button className='button-change-launch' onClick={next} id='buttonNext'>
         <i className="fas fa-chevron-right next"></i>
       </button>
     </div>
